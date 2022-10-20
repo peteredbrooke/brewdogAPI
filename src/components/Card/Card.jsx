41d91241c596f114678b13da5f951ff87052ee0d
peteredbrooke <peter.john.edbrooke@gmail.com>
2022-10-20 15:04:43 +0100

    more changes to styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,17 +7,13 @@ const Card = (props) => {
     return (
         <div className={styles.card}>
             <img className={styles.beerImage} src={image} />
-            
             <div className={styles.cardTitle}>
                 <h1>{name}</h1>
                 <h3>"{tagline}"</h3>
             </div>
-            
             <div className={styles.textSpace}>
                 <p>{description}</p>
-
             </div>
-            
             <h4> ABV {abv} %</h4>
 
         </div>
